aa5b3fe719dd4598ad397c0540ea667cb73b9bd2
VatiMorales <vtmorales@misena.edu.co>
2021-08-16 22:31:49 -0500

    Añadi CRUD con diseño del Condicionamiento

---
+++ resources/views/condicionamientos/create.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.base')
+@section('title', 'Crear condicionamiento')
+@section('content')
+
+<head>
+	<link rel="stylesheet" href="{{ asset('estilos/fichas/estilos.css') }}">
+</head>
+	<h1>Crear condicionamiento</h1>
+	<form action="/condicionamientos" method="post" enctype="multipart/form-data"id="formulario">
+		@csrf
+		<div class="form-group">
+			<label for="SC_CondicionamientoMatricula_Descripcion">Descripción</label>
+			<textarea class="form-control" rows="2"name="SC_CondicionamientoMatricula_Descripcion" id="SC_CondicionamientoMatricula_Descripcion"value="{{old('SC_CondicionamientoMatricula_Descripcion')}}"></textarea>
+			@error('SC_CondicionamientoMatricula_Descripcion')
+				<small>{{$message}}</small>
+			@enderror
+		</div>
+
+		<div class="form-group">
+			<label for="SC_CondicionamientoMatricula_Fecha">Fecha</label>
+			<input type="date" name="SC_CondicionamientoMatricula_Fecha" id="SC_CondicionamientoMatricula_Fecha" class="form-control" value="{{old('SC_CondicionamientoMatricula_Fecha')}}">
+			@error('SC_CondicionamientoMatricula_Fecha')
+				<small>{{$message}}</small>
+			@enderror
+		</div>
+		<div class="form-group">
+			<label for="SC_CondicionamientoMatricula_FechaMaxima">Fecha máxima</label>
+			<input type="date" name="SC_CondicionamientoMatricula_FechaMaxima" id="SC_CondicionamientoMatricula_FechaMaxima" class="form-control" value="{{old('SC_CondicionamientoMatricula_FechaMaxima')}}">
+			@error('SC_CondicionamientoMatricula_FechaMaxima')
+				<small>{{$message}}</small>
+			@enderror
+		</div>
+		
+		<div class="form-group">
+			<label for="SC_CondicionamientoMatricula_EvidenciasNoPresentadas">Evidencias no presentadas</label>
+			<input type="text" name="SC_CondicionamientoMatricula_EvidenciasNoPresentadas" id="SC_CondicionamientoMatricula_EvidenciasNoPresentadas" class="form-control" value="{{old('SC_CondicionamientoMatricula_EvidenciasNoPresentadas')}}">
+			@error('SC_CondicionamientoMatricula_EvidenciasNoPresentadas')
+				<small>{{$message}}</small>
+			@enderror
+		</div>
+
+		<div class="form-group">
+			<label for="SC_Acto_Administrativo_FK_ID">Acto administrativo</label>
+			<select name="SC_Acto_Administrativo_FK_ID" id="SC_Acto_Administrativo_FK_ID" class="form-control">
+				@foreach($actoadministrativo as $actoadministrativo)
+				<option value="{{$actoadministrativo->SC_ActoAdministrativoSanciones_PK_Id}}">{{$actoadministrativo->SC_ActoAdministrativoSanciones_DescripcionHechos}}</option>
+				@endforeach
+			</select>
+			@error('SC_Acto_Administrativo_FK_ID')
+				<small>{{$message}}</small>
+			@enderror
+		</div>
+
+
+		<button type="submit" class="btn btn-success" id="btncrear">Crear</button>
+	</form>
+@endsection 
--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> 6c17897f5224f65fcc9e6b9773f326f5a346a8dc
 @extends('layouts.base')
 @section('title','Inicio')
 @section('content')
