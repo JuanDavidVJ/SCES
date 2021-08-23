ece07841c5310aa3b32457b0042b79b9b11fbe9c
DanielS <daessar12@gmail.com>
2021-08-23 12:10:13 -0500

    Se arregla el borde del navbar del inverso

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <!-- Our Custom CSS -->
     <link rel="stylesheet" href="{{ asset('estilos/slidebar-navbar.css') }}">
-    <link rel="stylesheet" href="{{ asset('estilos/landing_page.css') }}">
 	<link rel="stylesheet" href="{{ asset('estilos/estilos.css') }}">
 
     <!-- Font Awesome JS -->
