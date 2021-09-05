2f8a642c93cbb770f85d67b1d06abdaf5c264992
JuanDavidVJ <juandavelezjara21@gmail.com>
2021-09-05 21:16:29 +0100

    Diseño implementado en Acta Comite

--- resources/views/ActaComite/create.blade.php
+++ resources/views/ActaComite/create.blade.php
@@ -1,99 +1,113 @@
 @extends('layouts.base')
 @section('title', 'Crear Acta de comite')
 @section('content')
-	<h1>Crear Acta de comite</h1>
-	<form action="/ActaComite" method="post" enctype="multipart/form-data">
+<head>
+	<link rel="stylesheet" href="{{ asset('estilos/estilos.css') }}">
+</head>
+
+<div class="container">
+		<h1>Crear Acta de Comite</h1>
+		<form action="/ActaComite" method="post" enctype="multipart/form-data">
 		@csrf
+		<div class="form-group">
+		<span class="input-group-text">Numero del acta</span>
+				<select name="Citacion" id="Citacion" class="form-control" style="font-size: 0.9em;">
+					@foreach($citaciones as $citacion)
+						<option value="{{$citacion->SC_CitacionPK_Id }}">{{$citacion->SC_Citacion_NumeroActa }}</option>
+					@endforeach
+					</select>
+					@error('SC_Novedades_Fecha')
+                <small style="color: red;">{{ $message }}</small>
+                @enderror
+		</div>
+
 	<div class="form-group">
-		<label for="Citacion">Numero del acta</label>
-		<select name="Citacion" id="Citacion" class="form-control" style="font-size: 0.9em;">
-		@foreach($citaciones as $citacion)
-			<option value="{{$citacion->SC_CitacionPK_Id }}">{{$citacion->SC_Citacion_NumeroActa }}</option>
-		@endforeach
-		</select>
-		@error('SC_Citacion_FK')
-		<small>{{$message}}</small>
-		@enderror
+	<span class="input-group-text">Nombre del Comité o de la Reunión</span>
+	   		 <input type="text" class="form-control" id="Nombre" name="Nombre"> 
+			@error('SC_Novedades_Fecha')
+                <small style="color: red;">{{ $message }}</small>
+			@enderror
+	  </div>
+
+	  <div class="form-group">
+	  <span class="input-group-text">Ciudad</span>
+	   		 <input type="text" class="form-control" id="Ciudad" name="Ciudad"> 
+	    	@error('SC_Novedades_Fecha')
+                <small style="color: red;">{{ $message }}</small>
+			@enderror
+	  </div>
+
+	  <div class="form-group">
+	  <span class="input-group-text">Fecha</span>
+	  			<input type="date" class="form-control" id="Fecha" name="Fecha">
+	  		@error('SC_Novedades_Fecha')
+                <small style="color: red;">{{ $message }}</small>
+			@enderror
+	  </div>
+
+	<div class="form-group">
+	<span class="input-group-text">Hora de inicio</span>
+				<input type="time" class="form-control" id="HoraInicio" name="HoraInicio">
+			@error('SC_Novedades_Fecha')
+                <small style="color: red;">{{ $message }}</small>
+			@enderror
 	</div>
 
 	<div class="form-group">
-	    <label for="Nombre">Nombre del Comité o de la Reunión</label>
-	    <input type="text" class="form-control" id="Nombre" name="Nombre"> 
-	    @error('SC_ActaComite_Nombre')
-	    	<small>{{ $message }}</small>
-	    @enderror
+	<span class="input-group-text">Hora de finalización</span>
+				<input type="time" class="form-control" id="HoraFin" name="HoraFin">
+			@error('SC_Novedades_Fecha')
+                <small style="color: red;">{{ $message }}</small>
+			@enderror
+	</div>
+	<div class="form-group">
+	<span class="input-group-text">Declaraciones del Aprendiz</span>
+				<input type="text" class="form-control" id="DeclaracionA" name="DeclaracionA">
+			@error('SC_Novedades_Fecha')
+                <small style="color: red;">{{ $message }}</small>
+			@enderror
+	</div>
+
+	<div class="form-group">
+	<span class="input-group-text">Declaraciones del responsable que solicito comité</span>
+				<input type="text" class="form-control" id="DeclaracionR" name="DeclaracionR">
+			@error('SC_Novedades_Fecha')
+                <small style="color: red;">{{ $message }}</small>
+			@enderror
+	</div>
+
+	<div class="form-group">
+	<span class="input-group-text">Otras declaraciones</span>
+				<input type="text" class="form-control" id="DeclaracionO" name="DeclaracionO">
+			@error('SC_Novedades_Fecha')
+                <small style="color: red;">{{ $message }}</small>
+			@enderror
+	</div>
+
+	<div class="form-group">
+	<span class="input-group-text">Asistentes</span>
+				<input type="text-area" class="form-control" id="Asistente" name="Asistente">
+			@error('SC_Novedades_Fecha')
+                <small style="color: red;">{{ $message }}</small>
+			@enderror
+	</div>
+
+	<div class="form-group">
+	<span class="input-group-text">Descargos del Aprendiz</span>
+				<input type="text-area" class="form-control" id="Descargo" name="Descargo">
+			@error('SC_Novedades_Fecha')
+                <small style="color: red;">{{ $message }}</small>
+			@enderror
+	</div>
+
+	<div class="form-group">
+	<span class="input-group-text">Desición</span>
+				<input type="text-area" class="form-control" id="Desicion" name="Desicion">
+			@error('SC_Novedades_Fecha')
+                <small style="color: red;">{{ $message }}</small>
+			@enderror
 	  </div>
-	  <div class="form-group">
-	    <label for="Ciudad">Ciudad</label>
-	    <input type="text" class="form-control" id="Ciudad" name="Ciudad"> 
-	    @error('SC_ActaComite_Ciudad')
-	    	<small>{{ $message }}</small>
-	    @enderror
-	  </div>
-	  <div class="form-group">
-	  	<label for="Fecha">Fecha</label>
-	  	<input type="date" class="form-control" id="Fecha" name="Fecha">
-	  	@error('SC_ActaComite_Fecha')
-	    	<small>{{ $message }}</small>
-	    @enderror
-	  </div>
-	<div class="form-group">
-		<label for="HoraInicio">Hora de inicio</label>
-		<input type="time" class="form-control" id="HoraInicio" name="HoraInicio">
-		@error('SC_ActaComite_HoraInicio')
-		  <small>{{ $message }}</small>
-	  @enderror
-	</div>
-	<div class="form-group">
-		<label for="HoraFin">Hora de finalización</label>
-		<input type="time" class="form-control" id="HoraFin" name="HoraFin">
-		@error('SC_ActaComite_HoraFin')
-		  <small>{{ $message }}</small>
-	  @enderror
-	</div>
-	<div class="form-group">
-		<label for="DeclaracionA">Declaraciones del Aprendiz</label>
-		<input type="text" class="form-control" id="DeclaracionA" name="DeclaracionA">
-		@error('SC_ActaComite_DeclaracionesAprendiz')
-		  <small>{{ $message }}</small>
-	  @enderror
-	</div>
-	<div class="form-group">
-		<label for="DeclaracionR">Declaraciones del responsable que solicito comité</label>
-		<input type="text" class="form-control" id="DeclaracionR" name="DeclaracionR">
-		@error('SC_ActaComite_DeclaracionesResponsable')
-		  <small>{{ $message }}</small>
-	  @enderror
-	</div>
-	<div class="form-group">
-		<label for="DeclaracionO">Otras declaraciones</label>
-		<input type="text" class="form-control" id="DeclaracionO" name="DeclaracionO">
-		@error('SC_ActaComite_DeclaracionesOtros')
-		  <small>{{ $message }}</small>
-	  @enderror
-	</div>
-	<div class="form-group">
-		<label for="Asistente">Asistentes</label>
-		<input type="text-area" class="form-control" id="Asistente" name="Asistente">
-		@error('SC_ActaComite_Asistentes')
-		  <small>{{ $message }}</small>
-	  @enderror
-	</div>
-	<div class="form-group">
-		<label for="Descargo">Descargos del Aprendiz</label>
-		<input type="text-area" class="form-control" id="Descargo" name="Descargo">
-		@error('SC_ActaComite_Descargos')
-		  <small>{{ $message }}</small>
-	  @enderror
-	</div>
-	<div class="form-group">
-		<label for="Desicion">Desición</label>
-		<input type="text-area" class="form-control" id="Desicion" name="Desicion">
-		@error('SC_ActaComite_Decision')
-		  <small>{{ $message }}</small>
-	  @enderror
-	</div>
-	
-	  <button type="submit" class="btn btn-primary">Crear</button>
+	  <button type="submit" class="btn btn-success mb-5" id="btncrear">Crear</button>
 	</form>
+</div>
 @endsection
