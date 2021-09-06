2978ddba28a987e46569033e74104263dbce4e36
GiselaCS <giselacriollo16@gmail.com>
2021-09-06 08:39:01 -0500

    se agrego el modelo de tipo Plan

--- app/Models/ActoAdministrativo.php
+++ app/Models/ActoAdministrativo.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//esto es lo mismo que la tabla notificaciones de la BD
 class ActoAdministrativo extends Model
 {
     use HasFactory;
-    protected $table ="sc_actoadministrativosanciones";
-    protected $primaryKey = "SC_ActoAdministrativoSanciones_PK_Id";
+    protected $table ="sc_notificaciones";
+    protected $primaryKey = "SC_Notificacion_ID";
 }
+// conection whit 
+   /* public function TipoPlan(){
+        return $this->belongsTo(Aprendiz::class, 'SC_Aprendiz_FK', 'SC_Aprendiz_PK_ID');
+    }*/
---
+++ app/Models/TipoPlan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoPlan extends Model
+{
+    use HasFactory;
+    use HasFactory;
+    protected $table ="sc_tipoplan";
+    protected $primaryKey = "SC_TipoPlan_ID";
+}
