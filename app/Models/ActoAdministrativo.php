0157711791d49162947f8f5bd19f9e99d44f9d4b
GiselaCS <giselacriollo16@gmail.com>
2021-09-06 09:03:50 -0500

    cambios en el model Acto A y sus relaciones

--- app/Models/ActoAdministrativo.php
+++ app/Models/ActoAdministrativo.php
@@ -12,7 +12,16 @@ class ActoAdministrativo extends Model
     protected $table ="sc_notificaciones";
     protected $primaryKey = "SC_Notificacion_ID";
 }
-// conection whit 
-   /* public function TipoPlan(){
-        return $this->belongsTo(Aprendiz::class, 'SC_Aprendiz_FK', 'SC_Aprendiz_PK_ID');
-    }*/
+// conection whit tipo plan
+    public function TipoPlan(){
+        return $this->belongsTo(TipoPlan::class, 'SC_Notifiacion_TipoPlan', 'SC_TipoPlan_ID');
+    }
+
+// conection whit  Acta de comite
+    public function Acta(){
+        return $this->belongsTo(ActaComite::class, 'SC_ActaComite_FK', 'SC_ActaComite_PK_ID');
+    }
+// conection whit tipo de notifiacion
+    public function TipoNotifiacion(){
+        return $this->belongsTo(TipoNotifiacion::class, 'SC_TipoNotificacion_FK', 'SC_TipoNotificacion_ID');
+    }
--- app/Models/TipoPlan.php
+++ app/Models/TipoPlan.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoPlan extends Model
 {
-    use HasFactory;
+  
     use HasFactory;
     protected $table ="sc_tipoplan";
     protected $primaryKey = "SC_TipoPlan_ID";
 }
+public function ActoAdministrativo(){
+        return $this->hasMany(ActoAdministrativo::class, 'SC_Notifacion_ID', 'SC_TipoPlan_ID');
+    }
