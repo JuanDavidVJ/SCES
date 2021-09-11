abb08ff55c4a43c8951542c9855d5a7a6a61bb10
JaimeCardona99 <andreskrdona06@gmail.com>
2021-09-10 20:37:55 -0500

    sincronizar

--- resources/views/actoadministrativo/index.blade.php
+++ resources/views/actoadministrativo/index.blade.php
@@ -18,17 +18,10 @@
 	@endif
   <table class="table table-striped">
     <tr>
-<<<<<<< HEAD
       <th scope="col">Fecha Inicial</th>
       <th scope="col">Fecha Limite</th>
       <th scope="col">Forma de envió</th>
       <th scope="col">Funcionarió a cargo</th>
-=======
-      <th scope="col">ID</th>
-      
-      <th scope="col">Tipo plan</th>
-      <th scope="col">Tipo notificacion</th>
->>>>>>> 35da892697719878ff9c54b4b6bdc9be3955fde7
       <th scope="col">Opciones</th>
     </tr>
 	@if(count($actoas)<=0)
@@ -38,19 +31,10 @@
 	@else
     @foreach($actoas as $actoas)
         		<tr>
-<<<<<<< HEAD
         		  <td class="descripcion">{{$actoas->SC_Notificacion_FechaInicial}}</td>
                   <td class="descripcion">{{$actoas->SC_Notificacion_FechaLimite}} </td>
                   <td class="descripcion">{{$actoas->SC_Notificacion_Forma}}</td>
 		          <td class="descripcion">{{$actoas->SC_Notificacion_Funcionario }}</td>
-=======
-        		  <td>{{$actoas->SC_Notificacion_ID}}</td>
-        		 
-				
-
-                  <td class="descripcion">{{$actoas->TipoP->SC_TipoPlan_Descripcion}}</td>
-		          <td>{{$actoas->TipoN->SC_TipoNotificacion_Descripcion }}</td>
->>>>>>> 35da892697719878ff9c54b4b6bdc9be3955fde7
 
 	                <td scope="col">
 			          	<a href="/actoadministrativo/{{$actoas->SC_Notificacion_ID}}" class="btn btn-outline-default"><i class="fas fa-eye"></i></a>
